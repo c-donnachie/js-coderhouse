cb85a170f2705117a87d7f61dcea6d5bcbd041e4
Cristian Donnachie <119471458+c-donnachie@users.noreply.github.com>
2023-09-26 01:10:46 -0300

    add min and max on input

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,30 @@
 function calcularPromedio() {
-    let notas = [];
-    const cantidadNotasInput = document.getElementById("cantidadNotas");
-    const cantidadNotas = parseInt(cantidadNotasInput.value);
+  let notas = []
+  const cantidadNotasInput = document.getElementById("cantidadNotas")
+    const cantidadNotas = parseInt(cantidadNotasInput.value)
+    
+  for (let i = 0; i < cantidadNotas; i++) {
+    let nota = parseFloat(prompt(`Ingresa la nota número ${i + 1}`))
 
-    for (let i = 0; i < cantidadNotas; i++) {
-        let nota = parseFloat(prompt(`Ingresa la nota número ${i + 1}`));
-
-        if (!isNaN(nota)) {
-            notas.push(nota);
-            console.log(nota);
-        } else {
-            alert("Por favor, ingresa un número válido.");
-            i--;
-        }
+    if (!isNaN(nota)) {
+      notas.push(nota)
+      console.log(nota)
+    } else {
+      alert("Por favor, ingresa un número válido.")
+      i--
     }
+  }
 
-    let suma = 0;
-    for (let i = 0; i < notas.length; i++) {
-        suma += notas[i];
-    }
+  let suma = 0
+  for (let i = 0; i < notas.length; i++) {
+    suma += notas[i]
+  }
 
-    let promedio = suma / notas.length;
+  let promedio = suma / notas.length
 
-    const resultadoDiv = document.getElementById("resultado");
-    resultadoDiv.innerHTML = "Notas ingresadas: " + notas.join(", ") + "<br>";
-    resultadoDiv.innerHTML += "Promedio: " + promedio.toFixed(2);
+  const resultadoDiv = document.getElementById("resultado")
+  resultadoDiv.innerHTML = "Notas ingresadas: " + notas.join(", ") + "<br>"
+  resultadoDiv.innerHTML += "Promedio: " + promedio.toFixed(2)
 }
 
-document.getElementById("calcularButton").addEventListener("click", calcularPromedio);
+document.getElementById("calcularButton").addEventListener("click", calcularPromedio)
