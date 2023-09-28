5c0fa86a3c59855c4d84b1cb487fdfc8753d667b
Cristian Donnachie <119471458+c-donnachie@users.noreply.github.com>
2023-09-28 17:21:23 -0300

    add animations and others

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 function calcularPromedio() {
   let notas = []
   const cantidadNotasInput = document.getElementById("cantidadNotas")
-    const cantidadNotas = parseInt(cantidadNotasInput.value)
-    
+  const cantidadNotas = parseInt(cantidadNotasInput.value)
+
   for (let i = 0; i < cantidadNotas; i++) {
     let nota = parseFloat(prompt(`Ingresa la nota número ${i + 1}`))
 
@@ -21,10 +21,53 @@ function calcularPromedio() {
   }
 
   let promedio = suma / notas.length
+  let estado = ""
 
-  const resultadoDiv = document.getElementById("resultado")
-  resultadoDiv.innerHTML = "Notas ingresadas: " + notas.join(", ") + "<br>"
-  resultadoDiv.innerHTML += "Promedio: " + promedio.toFixed(2)
+  const  x = document.getElementById("resultado-estado")
+
+  // animaciones lotties
+  const aprobado = document.getElementById("lottie-aprobado")
+  const reprobado = document.getElementById("lottie-reprobado")
+
+  //border of container
+  const border = document.getElementById("border")
+
+  function resultado() {
+    if (estado !== "") {
+      x.style.display = "block"
+    }
+  }
+
+  if (promedio === 100.0) {
+    estado = " Perfecto 🥳"
+    x.style.borderColor = "#00DDB3"
+    aprobado.style.display = "block"
+    border.style.borderColor = "#00DDB3"
+  } else if (promedio >= 90.0) {
+    estado = " Distinguido 🤩"
+    x.style.borderColor = "#00DDB3"
+  } else if (promedio >= 76.0) {
+    estado = " Bueno 😎"
+    x.style.borderColor = "#00DDB3"
+  } else if (promedio >= 60.0) {
+    estado = " Suficiente 😁"
+    x.style.borderColor = "#00DDB3"
+  } else {
+    estado = " Reprobado 🥲"
+    x.style.borderColor = "rgb(245, 44, 44)"
+    reprobado.style.display = "block"
+    border.style.borderColor = 'rgb(245, 44, 44)'
+  }
+
+  resultado()
+
+  const resultadoDivNotas = document.getElementById("resultado-notas")
+  const resultadoDivPromedio = document.getElementById("resultado-promedio")
+  const resultadoDivEstado = document.getElementById("resultado-estado")
+
+  resultadoDivNotas.innerHTML = "Notas ingresadas: " + notas.join(", ")
+  resultadoDivPromedio.innerHTML = "Promedio: " + promedio.toFixed(2)
+  resultadoDivEstado.innerHTML = estado
 }
 
 document.getElementById("calcularButton").addEventListener("click", calcularPromedio)
