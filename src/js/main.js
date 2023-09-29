4e55a95e8b1d07f998eb744fc98ba8f4d175284e
Cristian Donnachie <119471458+c-donnachie@users.noreply.github.com>
2023-09-28 23:08:26 -0300

    validations added

--- src/js/main.js
+++ src/js/main.js
@@ -10,15 +10,24 @@ function calcularPromedio() {
   const cantidadNotas = parseInt(cantidadNotasInput.value)
   const notas = []
   let suma = 0
+  let nota2 = 0
 
   for (let i = 0; i < cantidadNotas; i++) {
     let nota = parseFloat(prompt(`Ingresa la nota número ${i + 1}`))
 
+    nota2 = nota >= 10 ? nota : nota.toFixed(1)
+
     if (isNaN(nota)) {
       alert("Por favor, ingresa un número válido.")
       i--
+    } else if (nota > 10) {
+      alert("La nota maxima es un 10!")
+      i--
+    } else if (nota < 1) {
+      alert("La nota no puede ser menor a 1!")
+      i--
     } else {
-      notas.push(nota.toFixed(1))
+      notas.push(nota)
       suma += nota
     }
   }
@@ -26,8 +35,6 @@ function calcularPromedio() {
   const promedio = notas.length > 0 ? suma / notas.length : 0
   let estadoMensaje = ""
   let estado = ""
-
-  console.log(promedio)
 
   if (promedio === 10) {
     estado = "aprobado"
@@ -47,7 +54,7 @@ function calcularPromedio() {
   }
 
   resultadoDivNotas.innerHTML = "Notas ingresadas: " + notas.join(", ")
-  resultadoDivPromedio.innerHTML = "Promedio: " + promedio.toFixed(1)
+  resultadoDivPromedio.innerHTML = "Promedio: " + promedio
   resultadoDivEstado.innerHTML = estadoMensaje
 
   aprobado.style.display = estado === "aprobado" ? "block" : "none"
